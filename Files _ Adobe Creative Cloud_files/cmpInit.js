f0fb60e87989c0029453987b2bbd4687b6984ada
Te_Smith <93007222+Mon-TeSmith@users.noreply.github.com>
2022-01-08 15:17:02 -0600

    new workspace

---
+++ Files _ Adobe Creative Cloud_files/cmpInit.js
@@ -0,0 +1,4 @@
+window.fedsConfig = window.fedsConfig || {};
+window.fedsConfig.privacy = window.fedsConfig.privacy || {};
+window.fedsConfig.privacy.otDomainId = '7a5eb705-95ed-4cc4-a11d-0cc5760e93db'; // Add -test for stage domain.
+window.fedsConfig.privacy.footerLinkSelector = '#footer .cookies';
